e729aacab2078b9d2fa61d7a23ea8a8552467288
Brian Findley <bfindley@mymail.mines.edu>
2011-01-28 18:22:09 -0700

    Moved cart from its own page to the sidebar. Finished iteration D1

--- project02/app/controllers/store_controller.rb
+++ project02/app/controllers/store_controller.rb
@@ -7,6 +7,7 @@ class StoreController < ApplicationController
 		product = Product.find(params[:id])
 		@cart=find_cart
 		@cart.add_product(product)
+		redirect_to_index
 		rescue ActiveRecord::RecordNotFound
 			logger.error("Attempt to access invalid product #{params[:id]}")
 			redirect_to_index("Invalid product")
@@ -17,9 +18,14 @@ class StoreController < ApplicationController
 		redirect_to_index("Your cart is currently empty")
 	end
 	
+	def index
+		@products = Product.find_products_for_sale
+		@cart = find_cart
+	end
+	
 	private
-		def redirect_to_index(msg)
-			flash[:notice] = msg
+		def redirect_to_index(msg = nil)
+			flash[:notice] = msg if msg
 			redirect_to :action => 'index'
 		end
 	
